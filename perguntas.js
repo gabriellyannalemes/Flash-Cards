e618aab13ec790de90f40197ec990d51476f15ab
gabriellyannalemes <gabrielly.anna.lemes@escola.pr.gov.br>
2026-08-31 13:30:15 +0000

    .

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao (
+    'Programação',
+    'Para que serve o main de uma pagina html?',
+    'Serve para eu anexar todo conteúdo principal do meu site'
+)
+
+criaCartao (
+    'Física',
+    'O que é energia cinética?',
+    'É a energia associada ao movimento de um corpo.'
+)
+
+criaCartao (
+    'Lingua Portuguesa',
+    'O que é uma oração subordinada adverbial?',
+    'É uma oração que exerce função de advérbio em relação à oração principal, indicando circunstâncias como causa, condição, tempo, finalidade etc..'
+)
+
+criaCartao (
+    'Geografia',
+    'Quais são as estações do ano ?',
+    'Outono, Inverno, Verão, Primavera'
+)
+
+criaCartao (
+    'Empreendedorismo',
+    'O que é MEI?',
+    'Microempreendedorismo individual '
+)
+
+criaCartao (
+    'Empreendedorismo',
+    'O que é receita recorrente?',
+    'Fluxo continuo de faturamento gerado para a empresa'
+)
+
+criaCartao (
+    'Empreendedorismo',
+    'O que e previsibilidade?',
+    'qualidade da pessoa que se comporta da forma como se espera'
+)
+
+criaCartao (
+    'Empreendedorismo',
+    'Quais são as vantagens de um negócio ser escalável?',
+    'as empresa s adotam melhorias nos processos e conquistam mais credibilidade para a marca'
+)
+
+criaCartao (
+    'Biologia',
+    'Quais são os tipos de agrotóxicos?',
+    'Inseticidas, fungicidas, herbicidas, esfoliantes e fumigantes'
+)
+
+criaCartao (
+    'Biologia',
+    'Qual o conceito de saúde?',
+    'Um estado de completo bem-estar físico, mental e social, não apenas na ausência de doenças'
+)
